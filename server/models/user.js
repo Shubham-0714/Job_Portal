d7339750171f57b4976f6e1971add5574c6a09db
shubham7714 <shubhamaggarwal14@gmail.com>
2026-03-06 01:42:08 +0530

    Fix user role enum

--- server/models/user.js
+++ server/models/user.js
@@ -18,8 +18,8 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ["admin", "user"], 
-    default: "user",
+    enum: ["candidate", "recruiter"], 
+    default: "candidate",
   },
   createdAt: {
     type: Date,
